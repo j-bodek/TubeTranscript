d49c0fea0e46d7bd7a0d4abcc4b25a2cb0830da6
Liljack118 <kamilzajaczek4@gmail.com>
2024-06-16 16:53:14 +0200

    structurize crawler

--- main.py
+++ main.py
@@ -7,47 +7,11 @@ from os import path
 from pydub import AudioSegment
 import os
 import tempfile
+from typing import AsyncGenerator
+import logging
 
-
-def _get_stream(video) -> tuple[str, str]:
-    for i in range(1, 4):
-        try:
-            streams = video.streams
-            stream = streams.get_audio_only() or streams.first()
-            return stream, video.video_id
-        except Exception as e:
-            pass
-
-        if i != 2:
-            time.sleep(2**i)
-
-    return None, None
-
-
-async def get_stream(video) -> tuple[str, str]:
-    return await to_thread.run_sync(_get_stream, video)
-
-
-async def get_streams(channel):
-
-    tasks = []
-    for video in channel.videos:
-        tasks.append(get_stream(video))
-        if len(tasks) == 5:
-            for url, _id in await asyncio.gather(*tasks):
-                if not url:
-                    continue
-
-                yield url, _id
-
-            tasks = []
-
-    if tasks:
-        for url, _id in await asyncio.gather(*tasks):
-            if not url:
-                continue
-
-            yield url, _id
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 
 async def download_file(stream, output_path):
@@ -81,22 +45,83 @@ def _download_file(stream, output_path):
                     print(f"Error while transcribing - {e}")
 
 
-async def main():
-    url = "https://www.youtube.com/@squewe"
+class ChannelCrawler:
+    def __init__(
+        self, url: str, output_dir: str, batch: int = 5, background_processes: int = 3
+    ):
+        self.channel = pytube.Channel(url)
+        self.batch = batch
+        self.output_dir = self._output_dir(output_dir)
+        self.background_processes = background_processes
 
-    tasks = []
-    channel = pytube.Channel(url)
-
-    async for stream, _id in get_streams(channel):
-        _dir = f"files/{channel.channel_id}/"
+    def _output_dir(self, output_dir: str) -> str:
+        _dir = f"{output_dir.rstrip('/')}/{self.channel.channel_id}/"
         if not path.exists(_dir):
             os.makedirs(_dir)
 
-        tasks.append(download_file(stream, output_path=f"{_dir}{_id}.txt"))
+        return _dir
 
-        if len(tasks) == 3:
+    async def crawl(self):
+        tasks = []
+        async for _id, stream in self.list():
+
+            tasks.append(
+                download_file(stream, output_path=f"{self.output_dir}{_id}.txt")
+            )
+
+            if len(tasks) == self.background_processes:
+                await asyncio.gather(*tasks)
+                tasks = []
+
+        if len(tasks):
             await asyncio.gather(*tasks)
-            tasks = []
+
+    async def list(self) -> AsyncGenerator[str, pytube.Stream]:
+        """Generate ids and streams urls of the channel videos"""
+        tasks = []
+        for video in self.channel.videos:
+            tasks.append(self.get_stream(video))
+            if len(tasks) == self.batch:
+                for _id, stream in await asyncio.gather(*tasks):
+                    if not stream:
+                        continue
+
+                    yield _id, stream
+
+                tasks = []
+
+        if tasks:
+            for _id, stream in await asyncio.gather(*tasks):
+                if not stream:
+                    continue
+
+                yield _id, stream
+
+    async def get_stream(self, video) -> tuple[str, pytube.Stream]:
+        return await to_thread.run_sync(self._get_stream, video)
+
+    @staticmethod
+    def _get_stream(video) -> tuple[str, pytube.Stream]:
+        for i in range(1, 4):
+            try:
+                streams = video.streams
+                stream = streams.get_audio_only() or streams.first()
+                return video.video_id, stream
+            except Exception as e:
+                logging.error(f"Error getting video {video.video_id} - {e}")
+
+            if i != 2:
+                # add exponential backoff
+                time.sleep(2**i)
+
+        return None, None
+
+
+async def main():
+    url = "https://www.youtube.com/@squewe"
+
+    crawler = ChannelCrawler(url, output_dir="files/")
+    await crawler.crawl()
 
 
 if __name__ == "__main__":
